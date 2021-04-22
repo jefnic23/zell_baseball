3d4ba94a63b8fb8186d65b55c7f3fdf30ca5dd5d
Jeff Nicholas <jefnic23@gmail.com>
2021-04-22 15:23:45 -0400

    statsapi calls

--- statsapi.js
+++ statsapi.js
@@ -7,30 +7,125 @@ var date = `${mm}/${dd}/${yyyy}`;
 var base_url = "http://statsapi.mlb.com";
 var main_url = `http://statsapi.mlb.com/api/v1/schedule/games/?sportId=1&date=${date}&hydrate=lineups`;
 
-$.getJSON(main_url, function(data) {
-    console.log(data);
-    var games = data.dates[0].games;
-    for (var i=0; i < games.length; i++) {
-        if (games[i].status.detailedState != "Postponed") {
-            var away_team = games[i].teams.away.team.name;
-            var home_team = games[i].teams.home.team.name;
-            var away_lineup = games[i].lineups.awayPlayers;
-            var home_lineup = games[i].lineups.homePlayers;
-            console.log(`${away_team} @ ${home_team}`);
-            for (var j=0; j < away_lineup.length; j++) {
-                var away_player = base_url + away_lineup[j].link;
-                $.getJSON(away_player, function(data) {
-                    var away_name = data.people[0].fullName;
-                    var away_hand = data.people[0].batside.code;
-                    console.log(`${away_name} ${away_hand}`);
-                })
-                var home_player = base_url + home_lineup[j].link;
-                $.getJSON(home_player, function(data) {
-                    var home_name = data.people[0].fullName;
-                    var home_hand = data.people[0].batside.code;
-                    console.log(`${home_name} ${home_hand}`);
-                })
+var game_list = [];
+
+$.getJSON(main_url, function(result) {
+    //console.log(result);
+    $.each(result.dates[0].games, function(i, data) {
+        //console.log(data);
+        var game = {};
+        if (data.status.detailedState != "Postponed") {
+            var away_team = data.teams.away.team.name;
+            var home_team = data.teams.home.team.name;
+            game['away_team'] = away_team;
+            game['away_lineup'] = [];
+            game['away_pitcher'] = "TBD";
+            game['home_team'] = home_team;
+            game['home_lineup'] = [];
+            game['home_pitcher'] = "TBD";
+            game['scheduled_innings'] = data.scheduledInnings;
+            game['ump'] = "";
+
+            var live_feed = base_url + data.link;
+            $.getJSON(live_feed, function(data) {
+                //console.log(data);
+                game['game_time'] = data.gameData.datetime.time;
+                game['venue'] = data.gameData.venue;
+                game['weather'] = data.gameData.weather;
+
+                var pitchers = data.gameData.probablePitchers;
+                if (pitchers) {
+                    var away_pitcher = {};
+                    away_pitcher['name'] = pitchers.away.fullName;
+
+                    var home_pitcher = {};
+                    home_pitcher['name'] = pitchers.home.fullName;
+
+                    var away_link = base_url + pitchers.away.link;
+                    $.getJSON(away_link, function(data) {
+                        away_pitcher['hand'] = data.people[0].pitchHand.code;
+                    });
+
+                    var home_link = base_url + pitchers.away.link;
+                    $.getJSON(home_link, function(data) {
+                        home_pitcher['hand'] = data.people[0].pitchHand.code;
+                    });
+
+                    game['away_pitcher'] = away_pitcher;
+                    game['home_pitcher'] = home_pitcher;
+                }
+
+                var umps = data.liveData.boxscore.officials;
+                var ump = {};
+                $.each(umps, function(i, data) {
+                    if (data.officialType == "Home Plate") {
+                        ump['name'] = data.official.fullName;
+
+                        var ump_link = base_url + data.official.link;
+                        $.getJSON(ump_link, function(data) {
+                            ump['sz_bottom'] = data.people[0].strikeZoneBottom;
+                            ump['sz_top'] = data.people[0].strikeZoneTop;
+                        });
+                    }
+                });
+
+                game['ump'] = ump;
+            });
+
+            var lineups = data.lineups;
+            if (lineups) {
+                if (lineups.awayPlayers) {
+                    var away_lineup = lineups.awayPlayers;
+                    for (var j=0; j < away_lineup.length; j++) {
+                        var away_player = base_url + away_lineup[j].link;
+                        $.getJSON(away_player, function(result) {
+                            $.each(result.people, function(i, data) {
+                                var player = {};
+                                var name = data.fullName;
+                                var hand = data.batSide.code;
+                                player['name'] = name;
+                                player['hand'] = hand;
+                                game['away_lineup'].push(player);
+                            });
+                        });
+                    }
+                }
+                if (lineups.homePlayers) {
+                    var home_lineup = lineups.homePlayers;
+                    for (var j=0; j < lineups.homePlayers.length; j++) {
+                        var home_player = base_url + home_lineup[j].link;
+                        $.getJSON(home_player, function(result) {
+                            $.each(result.people, function(i, data) {
+                                var player = {};
+                                var name = data.fullName;
+                                var hand = data.batSide.code;
+                                player['name'] = name;
+                                player['hand'] = hand;
+                                game['home_lineup'].push(player);
+                            });
+                        });
+                    }
+                }
             }
         }
-    }
+        game_list.push(game);
+    });
 }); 
+
+console.log(game_list);
+
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://sports-odds-betapi.p.rapidapi.com/v1/sports/line/en",
+	"method": "GET",
+	"headers": {
+		"package": "4a788ec11cd42226e2fdcbd62253379c",
+		"x-rapidapi-key": "fd80fc833bmsh2b174af060c335fp1e8ec1jsneefa0b5408b1",
+		"x-rapidapi-host": "sports-odds-betapi.p.rapidapi.com"
+	}
+};
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
