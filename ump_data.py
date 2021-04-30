150046c9305305e0527f30c1fe3c2e84e5582340
Jeff Nicholas <jefnic23@gmail.com>
2021-04-30 01:08:55 -0400

    ump data

--- ump_data.py
+++ ump_data.py
@@ -3,30 +3,68 @@ import numpy as np
 import glob
 from sklearn.preprocessing import MinMaxScaler
 
-# make two ump dfs, one of 2021 and one of 2015-2020
-# if any umps are in both, use those umps in savant merge
+df1 = pd.read_csv('umps.csv')
 
-savant = pd.concat([pd.read_csv(f, engine='python') for f in glob.glob('C:/Users/jefni/Documents/Pitcher List/statcast_data/savant_20*.csv')])
-umps = pd.read_csv("C:/Users/jefni/Documents/Pitcher List/statcast_data/umpires_ids_game_pk.csv")
-umps.rename(columns = {'name': 'ump'}, inplace=True)
-umps = umps[umps['position'] == 'HP']
-df = pd.merge(savant[['description', 'plate_x', 'plate_z', 'game_pk']], umps[['ump', 'game_year', 'game_pk']], on='game_pk')
-df['active'] = np.where()
+df2 = pd.read_csv("E:/Documents/Pitcher List/statcast_data/umpires_ids_game_pk.csv")
+df2['game_date'] = df2['game_date'].str.split('-').str[0]
+df2 = df2[(df2['position'] == 'HP') & (df2['name'].isin(df2[df2['game_date'] == '2021']['name']))]
+df2 = df2.groupby('name')['game_pk'].count()
 
-def q5(x):
-    return x.quantile(0.05)
-def q95(x):
-    return x.quantile(0.95)
+df = pd.merge(df1, df2, on='name')
+df.to_csv('umps.csv')
+
+# umps = pd.read_csv("E:/Documents/Pitcher List/statcast_data/umpires_ids_game_pk.csv")
+# umps['game_date'] = umps['game_date'].str.split('-').str[0]
+# umps = umps[(umps['position'] == 'HP') & (umps['name'].isin(umps[umps['game_date'] == '2021']['name']))]
+# df = pd.concat([pd.read_csv(f, engine='python') for f in glob.glob('E:/Documents/Pitcher List/statcast_data/savant_20*.csv')])
+# df = pd.merge(df[['description', 'plate_x', 'plate_z', 'sz_bot', 'sz_top', 'game_pk']], umps[['name', 'game_date', 'game_pk']], on='game_pk')
+# df['ball'] = np.where((df['plate_x'] < -0.8308333) | 
+#                       (df['plate_x'] > 0.8308333) |
+#                       (df['plate_z'] < df['sz_bot']) |
+#                       (df['plate_z'] > df['sz_top']), 
+#                       1, 0
+#                       )
+# df['strike'] = np.where((df['plate_x'] > -0.8308333) & 
+#                         (df['plate_x'] < 0.8308333) &
+#                         (df['plate_z'] > df['sz_bot']) &
+#                         (df['plate_z'] < df['sz_top']), 
+#                         1, 0
+#                         )
+# df['correct_ball'] = np.where((df['description'] == 'ball') & 
+#                               (df['ball'] == 1),
+#                               1, 0
+#                               )
+# df['correct_strike'] = np.where((df['description'] == 'called_strike') & 
+#                                 (df['strike'] == 1),
+#                                 1, 0
+#                                 )
+# df_balls = df[df['description'] == 'ball'].groupby('name').agg({'correct_ball': 'mean'})
+# df_strikes = df[df['description'] == 'called_strike'].groupby('name').agg({'correct_strike': 'mean'})
+# df = pd.merge(df_balls, df_strikes, on="name")
+# df['ball_rank'] = (df['correct_ball'].mean() - df['correct_ball'])/ df['correct_ball'].std()
+# df['strike_rank'] = (df['correct_strike'] - df['correct_strike'].mean())/ df['correct_strike'].std()
+# df['score'] = ((df['ball_rank'] * (100 * (1 - df['correct_ball'].mean()))) + 
+#                df['strike_rank'] * (100 * (1 - df['correct_strike'].mean()))) / 2
+# scaler = MinMaxScaler(feature_range=(-1, 1))
+# df['runs'] = scaler.fit_transform(df['score'].to_numpy().reshape(-1,1))
+# df.to_csv('umps.csv')
+
+
+### old code, gets strike zone size by plate_x/z quantile ###
+
+# def q5(x):
+#     return x.quantile(0.05)
+# def q95(x):
+#     return x.quantile(0.95)
         
-df = df[df['description'] == "called_strike"].groupby("ump").agg(plate_x_5 = ('plate_x', q5),
-        plate_x_95 = ('plate_x', q95),
-        plate_z_5 = ('plate_z', q5),
-        plate_z_95 = ('plate_z', q95)
-        )
-df['plate_x'] = df['plate_x_95'] - df['plate_x_5']
-df['plate_z'] = df['plate_z_95'] - df['plate_z_5']
-df['total'] = df['plate_x'] + df['plate_z']
-df['zscore'] = (df['total'].mean() - df['total']) / df['total'].std()
-scaler = MinMaxScaler(feature_range=(-0.5, 0.5))
-df['runs'] = scaler.fit_transform(df['zscore'].to_numpy().reshape(-1,1))
-df.to_csv('umps.csv')    
+# df = df[df['description'] == "called_strike"].groupby("name").agg(plate_x_5 = ('plate_x', q5),
+#         plate_x_95 = ('plate_x', q95),
+#         plate_z_5 = ('plate_z', q5),
+#         plate_z_95 = ('plate_z', q95)
+#         )
+# df['plate_x'] = df['plate_x_95'] - df['plate_x_5']
+# df['plate_z'] = df['plate_z_95'] - df['plate_z_5']
+# df['total'] = df['plate_x'] + df['plate_z']
+# df['zscore'] = (df['total'].mean() - df['total']) / df['total'].std()
+# scaler = MinMaxScaler(feature_range=(-0.9, 1))
+# df['runs'] = scaler.fit_transform(df['zscore'].to_numpy().reshape(-1,1))
