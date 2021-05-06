7e0143a810d8d81504dc54428b625e01bf180c1d
Jeff Nicholas <jefnic23@gmail.com>
2021-05-06 14:05:58 -0400

    fixed weather

--- app/routes.py
+++ app/routes.py
@@ -20,8 +20,24 @@ def send_data(data):
     try:
         venue = data['game']['venue']
         ump = data['game']['ump']['official']['fullName']
+        temp = int(data['game']['weather']['temp'])
 
-        prediction = round(parks.loc[venue]['runs'] + umps.loc[ump]['runs'], 2)
+        if temp <= 46:
+            weather = -0.225
+        if 47 <= temp <= 53:
+            weather = -0.15
+        if 54 <= temp <= 62:
+            weather = -0.075
+        if 63 <= temp <= 71:
+            weather = 0.0
+        if 72 <= temp <= 79:
+            weather = 0.1
+        if 80 <= temp <= 87:
+            weather = 0.2
+        if temp >= 88:
+            weather = 0.3
+        
+        prediction = round(parks.loc[venue]['runs'] + umps.loc[ump]['runs'] + weather, 2)
 
         if data['game']['innings'] == 9:
             total = round(prediction - data['game']['over_under'], 2)
@@ -32,6 +48,7 @@ def send_data(data):
             bet = bets.loc[total]['bet']
         else:
             bet = "No bet"
+            
         emit('predictionData', {'gamePk': gamePk, 'game_time': game_time, 'prediction': prediction, 'total': total, 'bet': bet})
     except:
         emit('predictionData', {'gamePk': gamePk, 'game_time': game_time, 'prediction': "TBD", 'total': "TBD", 'bet': "TBD"})
