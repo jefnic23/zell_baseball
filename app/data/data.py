6556d225a7f7e26d72c02657f131bf280ed020fc
Jeff Nicholas <jefnic23@gmail.com>
2021-06-03 01:53:12 -0400

    bet multiplier

--- app/data/data.py
+++ app/data/data.py
@@ -44,7 +44,7 @@ def getBets():
     scaled = scaler.fit_transform(pd.Series(d['total']).to_numpy().reshape(-1, 1))
     for i, n in enumerate(d['total']):
         d['x'].append(scaled[i][0])
-        d['bet'].append(round(n * scaled[i][0] * 50, 1))  
+        d['bet'].append(round(n * scaled[i][0] * 60, 1))  
     df = pd.DataFrame(d, columns=d.keys())
     return df.to_csv('bets.csv', index=False)
 
@@ -208,7 +208,7 @@ def getParks():
     return df.to_csv('parks.csv')
 
 # getUmps()
-# getBets()
+getBets()
 # getFielding()
 # getBullpens()
 # getPitching()
