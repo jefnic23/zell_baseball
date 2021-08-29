f8c9f00af6eab59cb05cee39a7c69b9fcd381418
Jeff <jefnic23@gmail.com>
2021-08-29 17:14:15 -0400

    modifier to 5%

--- app/routes.py
+++ app/routes.py
@@ -182,7 +182,7 @@ def send_data(data):
         home_pvb = PvB(game['home_pitcher'], game['away_lineup'])
         away_matchups = getInnings(game['away_pitcher'], away_pvb, away_bullpen, innings)
         home_matchups = getInnings(game['home_pitcher'], home_pvb, home_bullpen, innings) 
-        prediction = (1.04 * venue) + handicap + ump + away_fielding + home_fielding + weather + away_matchups + home_matchups + wind
+        prediction = (1.05 * venue) + handicap + ump + away_fielding + home_fielding + weather + away_matchups + home_matchups + wind
         pred_data = [venue, handicap, weather, wind, ump, away_fielding, home_fielding, away_matchups, home_matchups]
 
         if line == 220:
