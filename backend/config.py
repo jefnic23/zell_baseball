4c48ef84cbb6fca63f74e23d96682dc7eef2e69c
Jeff Nicholas <jefnic23@gmail.com>
2023-04-14 17:49:53 -0400

    🚧 chore(config.py): add DEV_DATABASE_URL to Settings 🎉 feat(utils): add __init__.py The DEV_DATABASE_URL is added to the Settings class in config.py to allow for a separate development database. The __init__.py file is added to the utils directory to make it a package.

--- backend/config.py
+++ backend/config.py
@@ -4,6 +4,7 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     SECRET_KEY: str
     DATABASE_URL: str
+    DEV_DATABASE_URL: str
 
     class Config:
         env_file = ".env"
